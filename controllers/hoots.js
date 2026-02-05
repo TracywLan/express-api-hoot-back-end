b88c8d188ce72dd9b3825b274ccad425bad3545e
Tracywlan <tracylan12@gmail.com>
2026-02-05 12:04:13 -0500

    finished show and update route

--- controllers/hoots.js
+++ controllers/hoots.js
@@ -31,11 +31,56 @@ router.post('/', verifyToken, async(req, res) => {
 // GET	index	200	/hoots	List hoots
 
 router.get('/', verifyToken, async(req, res) => { // must be logged-in to see list of hoots
-    // use find to retrieve all hoots
+    try {// use find to retrieve all hoots
+        const hoot = await Hoot.find({})
+            .populate('author')
+            .sort({ createdAt: 'desc' });
+        res.status(200).json(hoot)
+    } catch (err) {
+        res.status(500).json({ err: err.message });
+    }
 })
 // GET	show	200	/hoots/:hootId	Get a single hoot
+router.get('/:hootId', verifyToken, async (req, res) => {
+    try {
+        const hoot = await Hoot.findById(req.params.hootId).populate('author');
+        res.status(200).json(hoot);
+    } catch (err) {
+        res.status(500).json({ err: err.message });
+    }
+})
 // PUT	update	200	/hoots/:hootId	Update a hoot
+router.put('/:hootId', verifyToken, async(req, res) => {
+    try {
+        // Find the hoot:
+        const hoot = await Hoot.findById(req.params.hootId);
+
+        // Check permissions:
+        if (!hoot.author.equals(req.user._id)) {
+            return res.status(403).send("You're not allowed to do that!")
+        }
+        
+
+        // Update hoot:
+        const updatedHoot = await Hoot.findByIdAndUpdate(
+            req.params.hootId,
+            req.body,
+            { new: true }
+        );
+
+        // Append req.user to the author property:
+        updatedHoot._doc.author = req.user;
+
+        // Issue JSON response:
+        res.status(200).json(updatedHoot);
+    } catch (err) {
+        res.status(500).json({ err: err.message });
+    }
+})
 // DELETE	deleteHoot	200	/hoots/:hootId	Delete a hoot
+router.delete('/:hootId', verifyToken, async(req, res) => {
+    
+})
 // POST	createComment	200	/hoots/:hootId/comments	Create a comment
 
 
